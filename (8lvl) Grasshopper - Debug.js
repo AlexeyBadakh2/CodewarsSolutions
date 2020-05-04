df12288ada20e7fe4d456ed4d004ad5404ee0634
AlexeyBadakh2 <alexeybadah2@gmail.com>
2020-05-04 21:38:21 +0300

    Add solution

---
+++ (8lvl) Grasshopper - Debug.js
@@ -0,0 +1,10 @@
+//Grasshopper - Debug
+//https://www.codewars.com/kata/55cb854deb36f11f130000e1
+function weatherInfo (temp) {
+    var celsius = (temp - 32) * (5/9);
+    if (celsius <= 0) {
+        return (celsius + " is freezing temperature");
+    } else{
+        return (celsius + " is above freezing temperature");
+    }
+}
